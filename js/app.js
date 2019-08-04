7c73eb7915b0f33f4963fdc77ee80cc7db97b4ea
Tsuzuku <gagandeepsandhu@protonmail.com>
2019-08-04 00:57:49 -0400

    Finished all the logic to play the game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-let phrase; 
+//Globals
+let phrase;  //Is the thing that needs to be guessed. 
+const  health = 10; //Is the players health static for now. 
+let currentHealth = health;
+let guesses = [];
+const blank = "___";
+const space = "   ";
+let phraseDiv;
+let livesDiv;
+let guessableCharacters = 0;
 
 const main = function () {
 
@@ -10,8 +19,20 @@ const main = function () {
 
     if(category === "champion") { phrase = championName(); }
 
+    phraseDiv = document.getElementById("phrase");
+    livesDiv = document.getElementById("hearts");
+
     //Initialize the board.
     initBoard();
+
+    let input = document.getElementById("guessInput");
+    //Add the onkey press to the input if not mobile
+    //if (!/Mobi|Android/i.test(navigator.userAgent)) {
+     //   input.addEventListener("keyup", function () { makeGuess(String.fromCharCode(event.keyCode)); } );
+    //}
+
+    let submit = document.getElementById("submit");
+    submit.addEventListener("click", function() { makeGuess(input.value); });
 };
 
 
@@ -64,22 +85,25 @@ const championSkin = function(champion = null) {
 };
 
 /**
- * Draws out the board with the appropriate blank spaces.
+ * Draws the appropriate ammount of Hearts.
  */
-const initBoard = function () {
-    const blank = "___";
-    const space = "   ";
-    const phraseDiv = document.getElementById("phrase");
-    const livesDiv = document.getElementById("hearts");
-
-
-    //Draw the hearts
-    for(let i = 0; i < 10; i++) { 
+const renderHealth = function() {
+    livesDiv.innerHTML = "";
+    for(let i = 0; i < currentHealth; i++) { 
         let heart = document.createElement("div");
         heart.classList.add("heart");
         
         livesDiv.appendChild(heart);
     }
+}
+
+/**
+ * Draws out the board with the appropriate blank spaces.
+ */
+const initBoard = function () {
+
+    //Draw the hearts
+    renderHealth();
 
     //Draw the words/letters/phrase
     for(let i = 0; i < phrase.length; i++) { 
@@ -91,13 +115,94 @@ const initBoard = function () {
 
             if(phrase.charAt(i) === "") { break; } // Phrase ended
 
-            let letter = document.createElement("h1");
-            letter.innerHTML = blank;
+            let letter = document.createElement("div");
+            let character = document.createElement("h1");
+            character.innerHTML = phrase.charAt(i);
+            character.classList.add("character");
+            letter.appendChild(character);
             letter.classList.add("letter");
-            word.appendChild(letter)
+            word.appendChild(letter);
+            guessableCharacters++;
             i++;
         }
         
         phraseDiv.appendChild(word);
+    }
+}
+
+/**
+ * This function reveals the answer 
+ */
+const revealAnswer = function () {
+    let wordList = phraseDiv.children;
+
+    for(let i = 0; i < wordList.length; i++) {
+        let characterList = wordList[i].children;
+        for(let j = 0; j < characterList.length; j++) {
+            characterList[j].children[0].style.display = "Block";
+        }
+    }
+}
+
+/**
+ * Disabled the Input, and Reads out the Message.
+ */
+const endGame = function(message) {
+    document.getElementById("guessInput").disabled = true;
+    alert(message);
+}
+
+/***
+ * This functions handles the logic for when a player enters
+ * a guess. 
+ * 
+ * @return: 
+ */
+const makeGuess = function(guess) {
+    let wordList = phraseDiv.children;
+    let badGuess = true;
+
+    //wipe input
+    let input = document.getElementById("guessInput");
+    input.value = "";
+
+    //Make sure the guess hasn't be done before. 
+    for(let k = 0; k < guesses.length; k++) {
+        if(guess.toLowerCase() === guesses[k]){
+            alert("Already Made this Guess");
+            return;
+        }
+    }
+
+    guesses.push(guess.toLowerCase()); //Push the new guess into the guesses list.
+    let character = document.createElement("h1");
+    character.innerHTML = guess;
+    document.getElementById("guesses").appendChild(character);
+
+    //Reveal the letters where the guess is correct. 
+    for(let i = 0; i < wordList.length; i++) {
+        let characterList = wordList[i].children;
+        for(let j = 0; j < characterList.length; j++) {
+            if(characterList[j].children[0].innerHTML.toLowerCase() === guess.toLowerCase()) {
+                badGuess = false;
+                characterList[j].children[0].style.display = "Block";
+
+                if(--guessableCharacters <= 0) {
+                    endGame("Victory!");
+                }
+            }
+        }
+    }
+
+    //If its an incorrect guess. update health. 
+    if(badGuess) {
+        currentHealth--;
+        renderHealth();
+
+        if(currentHealth === 0) { 
+            
+            endGame("Your Nexus Has Exploded.");
+            revealAnswer();
+        }
     }
 }
